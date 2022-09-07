cfa02b58ece06db2d4bd2aaaabaf2dd2c4da14c2
feel0321 <dishate@naver.com>
2022-09-07 19:06:43 +0900

    refactor: Button 클래스로 수정

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,22 @@
 import { $create } from "../../utils/dom.js";
 
-export default function Button({ $target, initText, onClick }) {
-  const $button = $create("button");
-  $target.appendChild($button);
+export default class Button {
+  constructor({ $target, initText, onClick }) {
+    const $button = $create("button");
 
-  this.render = () => {
-    $button.innerText = initText;
-  };
+    this.$button = $button;
+    this.text = initText;
+    this.onClick = onClick;
+    this.setEvent();
+    this.render();
+    $target.appendChild($button);
+  }
 
-  this.render();
+  setEvent() {
+    this.$button.addEventListener("click", this.onClick);
+  }
 
-  $button.addEventListener("click", onClick);
+  render() {
+    this.$button.innerText = this.text;
+  }
 }
